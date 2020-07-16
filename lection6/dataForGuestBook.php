82c91eb1a4bc07ea0fc9d05b4091bdbda79fc19f
happy-violence <cbeta-makapoba@yandex.ru>
2020-07-16 22:33:01 +0300

    добавила логику добавления записей в гостевую книгу

---
+++ lection6/dataForGuestBook.php
@@ -0,0 +1,18 @@
+<?php
+
+//require_once __DIR__ . '/function.php';
+require_once __DIR__ . '/classes.php';
+
+$guestBook = new GuestBook(__DIR__ . '/guestbook.txt');
+
+if (!empty($_POST['text'])) {
+    //необходимо получить массив уже имеющихся записей гостевой,
+    // добавить к этому массиву новый элемент (текст из формы) и
+    // записать полученный массив в тот же файл, разделяя элементы символом переноса строки
+    $guestBook->getData();
+    $guestBook->append($_POST['text']);
+    $guestBook->save();
+
+    header("Location: http://localhost:8000/guestBook.php");
+}
+
