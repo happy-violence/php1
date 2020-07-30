10b602ae260a3e6bbc0fa47e42c51027142ee6f8
happy-violence <cbeta-makapoba@yandex.ru>
2020-07-30 14:46:15 +0300

    исправила путь до файла

--- lection4/task2/data.php
+++ lection4/task2/data.php
@@ -1,10 +1,10 @@
 <?php
-
 if (!empty($_FILES)) {
     if (isset($_FILES['myimage'])) {
         if ($_FILES['myimage']['type'] === 'image/jpeg' || $_FILES['myimage']['type'] === 'image/png' ||
             $_FILES['myimage']['type'] === 'image/jpg') {
             if (0 == $_FILES['myimage'] ['error']) {
+                //Перемещает загруженный файл в новое место
                 move_uploaded_file(
                     $_FILES['myimage'] ['tmp_name'],
                     __DIR__ . '/images/' . $_FILES['myimage'] ['name']
