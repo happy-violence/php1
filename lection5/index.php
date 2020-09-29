3f78fc10a555955ae5eb416d206dd4a1357266e8
happy-violence <cbeta-makapoba@yandex.ru>
2020-09-29 21:29:23 +0300

    добавила комментарии

--- lection5/index.php
+++ lection5/index.php
@@ -6,7 +6,7 @@ require_once __DIR__. '/function.php';
 <html>
 <?php
 //позволяем загружать изображения в галерею только авторизованным пользователям:
-//если значение, возвращаемое фунцией, не null, то отображаем форму загрузки файлов
+//если значение, возвращаемое функцией, не null, то отображаем форму загрузки файлов
 if (!empty(getCurrentUser())) { ?>
     <form action="/data.php" method="post" enctype="multipart/form-data">
         <input type="file" name="myimage">
